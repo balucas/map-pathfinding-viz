85bbbde473798ac4857c45ea7149a312a193b005
Bernard Lucas <blucas1597@gmail.com>
2021-06-18 17:12:55 -0400

    refactored for new map data format

--- src/index.js
+++ src/index.js
@@ -13,12 +13,13 @@ const test = createDrawing(gl);
 
 const request = require("./request");
 let toronto;
-request("/data/toronto.json", {
+request("/data/newto.json", {
   responseType: 'json',
   progress: (x) => {console.log(x.percent)} 
 })
   .then((res) => {
     toronto = res;
+    main();
   });
 
 function render(time) {
@@ -27,39 +28,41 @@ function render(time) {
   test.draw(); 
 }
 
-let mapData = {
-  nodes: [
-    { x: 0.0, y:0.0},
-    { x: 100.0, y: 50.0},
-    { x: 50.0, y: 100.0}
-  ],
-  edges: [[0,1],[1,2],[2,0]]
+function main(){
+  let mapData = {
+    nodes: [
+      { x: 0.0, y:0.0},
+      { x: 100.0, y: 50.0},
+      { x: 50.0, y: 100.0}
+    ],
+    edges: [[0,1],[1,2],[2,0]]
+  }
+
+  mapData = toronto;
+
+  let nodes = mapData.nodes;
+  let edges = mapData.edges;
+  let verts = [];
+  let indices = [];
+
+  nodes.forEach(el => {
+    verts.push(el.x);
+    verts.push(el.y);
+  });
+
+  edges.forEach(el => {
+    indices.push(el[0]);
+    indices.push(el[1]);
+    
+  });
+
+  let color = [1.0, 1.0, 1.0, 1];
+
+  test.addObject(verts, indices, color);
+
+  requestAnimationFrame(render);
+
+  document.addEventListener("click", e => {
+    console.log("clicked!");
+  })
 }
-
-mapData = toronto;
-
-let nodes = mapData.nodes;
-let edges = mapData.edges;
-let verts = [];
-let indices = [];
-
-nodes.forEach(el => {
-  verts.push(el.x);
-  verts.push(el.y);
-});
-
-edges.forEach(el => {
-  indices.push(el[0]);
-  indices.push(el[1]);
-  
-});
-
-let color = [1.0, 1.0, 1.0, 1];
-
-test.addObject(verts, indices, color);
-
-requestAnimationFrame(render);
-
-document.addEventListener("click", e => {
-  console.log("clicked!");
-})
