9d0d4edb7ad3daa72f101784a513d87f203ea880
Bernard Lucas <blucas1597@gmail.com>
2021-07-03 14:48:56 -0400

    improved map pins

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,15 @@ module.exports = {
     baseMap: [1.0, 1.0, 1.0, 1],
     searchMap: [],
     path: [],
-    startPin: [0.2, 0.57, 0.969, 1],
-    targetPin: [0.75, 0.17, 0.17, 1],
+
+    // start pin colors
+    startPin: [0.05, 0.30, 0.669, 1],
+    startPin1: [0.2, 0.57, 0.969, 1],
+    startPin2: [1, 1, 1, 1],
+    
+    // target pin colors
+    targetPin: [0.45, 0.07, 0.07, 1],
+    targetPin1: [0.80, 0.17, 0.17, 1],
+    targetPin2: [0.75, 0.17, 0.17, 1],
   }
 }
